91e47098cc658ea9ef138b0d6c1fe98726e203fc
Sakshamkhadka7 <sakshamcode12@gmail.com>
2025-11-25 19:36:06 +0545

    Added login authentication using jsonwebtoken

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,57 @@
 import { User } from "../models/userModal.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const Login=async(req,res)=>{
+  
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(401).json({
+                message:"Invalid data",
+                success:"false"
+            })
+        };
+
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                message:"Invalid email or password",
+                success:false,
+            })
+        }
+
+        const isMatch=await bcryptjs.compare(password,user.password);
+        if(!isMatch){
+          return res.status(401).json({
+            message:"Invalid email or password",
+            success:"false",
+          })
+        };
+       
+        // const tokenData={
+        //     id:User._id,
+        // }
+
+
+        const token=await jwt.sign("token","asdfdsfsfsfdfdsfdsfdfd",{expiresIn:"1d"});
+        return res.status(200).cookie("token",tokenData,{httpOnly:true}).json({
+            message:`Welcome back ${User.fullName}`,
+            success:true,
+            
+        }); 
+        
+
+    }catch(error){
+        
+      console.log(error);
+       
+
+    }
+
+
+}
+
 
 export const Register = async (req, res) => {
   try {
