2c0420abe00e5e55af4a2028257c69addef4e825
Sakshamkhadka7 <sakshamcode12@gmail.com>
2025-11-28 21:56:08 +0545

    Added fetching movie API from tmdb in browse section

--- reactnetflix/src/components/Browse.jsx
+++ reactnetflix/src/components/Browse.jsx
@@ -4,15 +4,30 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import MainContainer from "./MainContainer";
 import MovieContainer from "./MovieContainer";
+import axios from "axios";
+import { Now_Playing_Movie, options } from "../utils/constant";
 
 const Browse = () => {
   const user = useSelector((store) => store.user.user);
   const navigate = useNavigate();
  
+ const nowPlaying = async () => {
+  try {
+    const res = await axios.get(Now_Playing_Movie, options);
+    console.log("Movies: ", res.data);
+  } catch (error) {
+    console.error("Error fetching movies:", error.response?.data || error);
+  }
+};
+
+
   useEffect(()=>{
   if (!user) {
     navigate("/");
   }
+
+  nowPlaying();
+   
   },[])
 
 
--- reactnetflix/src/utils/constant.js
+++ reactnetflix/src/utils/constant.js
@@ -1,1 +1,13 @@
 export const API_END_POINT="http://localhost:8080/api/v1/user";
+export const Now_Playing_Movie =
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+
+export const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOGNlMWVjM2Q1ZDgwOGU2MTkwMGEwODQ1MDAyYzRkZCIsIm5iZiI6MTc2NDM0Mzg2OS4zMTksInN1YiI6IjY5MjljMDNkNGE5Y2FjNWQyMTI0NWZhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vrjNXM4XP9mWtuz2ybaP5fPrmIRYFB7uoUpRT4Wh554'
+  }
+};
+
