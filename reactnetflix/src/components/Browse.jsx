ac03cc70bf9bfe60171104a3703fd50fbc098274
Sakshamkhadka7 <sakshamcode12@gmail.com>
2025-11-28 22:09:45 +0545

    Added seperate Slice for movies

--- reactnetflix/src/components/Browse.jsx
+++ reactnetflix/src/components/Browse.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import MainContainer from "./MainContainer";
 import MovieContainer from "./MovieContainer";
 import axios from "axios";
 import { Now_Playing_Movie, options } from "../utils/constant";
+import { getNowPlayingMovies } from "../redux/MovieSlice";
 
 const Browse = () => {
   const user = useSelector((store) => store.user.user);
   const navigate = useNavigate();
+  const dispatch=useDispatch();
  
  const nowPlaying = async () => {
   try {
     const res = await axios.get(Now_Playing_Movie, options);
-    console.log("Movies: ", res.data);
+    dispatch(getNowPlayingMovies(res.data.results));
   } catch (error) {
     console.error("Error fetching movies:", error.response?.data || error);
   }
